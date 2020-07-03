390dcd376daada97951f30c85182af2a3e5fa851
Stefan Cimander <stefan.cimander@me.com>
2020-07-03 13:42:17 +0200

    Add empty universal app SwiftUI Xcode project

---
+++ Shared/AthletyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AthletyApp.swift
+//  Shared
+//
+//  Created by Stefan Cimander on 03.07.20.
+//
+
+import SwiftUI
+
+@main
+struct AthletyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
